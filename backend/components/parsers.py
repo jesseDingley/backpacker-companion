762174f37419c5a14ce3137939493c8d2486d956
Jesse Dingley <jessedingley@Jesses-MacBook-Pro.local>
2025-04-05 14:31:54 +0700

    prompt improvements

--- backend/components/parsers.py
+++ backend/components/parsers.py
@@ -23,3 +23,25 @@ class Parsers:
     """
 
     off_topic_verification_parser = JsonOutputParser(pydantic_object=IsOffTopic)
+
+
+"""
+CONTEXT RELEVANCE
+
+    Options:
+        1. Context is relevant to the query
+            EX: query: "hostels in Paris"; ctxt: "Montmartre hostel"
+            => generate response
+        2. Context is empty and query is a conversational message
+            EX: query: "thanks"; ctxt: ""
+            => generate response
+        3. Context is empty and query is a question
+            EX: query: "hostels in Paris"; ctxt: ""
+            => generate apology ('i lack info')
+        4. Context is irrelevant to query
+            EX: query: "hostels in Paris": ctxt: "la rochelle hostel"
+            => generate apology ('i lack info')
+
+
+
+"""
